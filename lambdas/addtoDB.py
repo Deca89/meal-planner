237af9c1f8ad43dc0e368e35273908c675529654
Gosia <malgorzata.zaremba@gmail.com>
2023-01-03 11:53:57 +0200

    lambda for email and sns

--- lambdas/addtoDB.py
+++ lambdas/addtoDB.py
@@ -9,58 +9,22 @@ import uuid
 def lambda_handler(event, context):
     diet = event['queryStringParameters']['diet']
     email = event['queryStringParameters']['email']
-    id = str(uuid.uuid4())
     
-    toSend = {
-        'diet': diet,
-        'id': id,
-        'email': email,
-    }
+    client = boto3.client('sns')
 
-    data = boto3.client('dynamodb').put_item(TableName=os.environ['TableName'], Item=json.loads(toSend))
-
-    # bucket = event['Records'][0]['s3']['bucket']['name']
-    # json_file_name = event['Records'][0]['s3']['object']['key']
-    # json_object = s3_client.get_object(Bucket=bucket,Key=json_file_name)
-    # file_reader = json_object['Body'].read().decode("utf-8")
-    # file_reader = ast.literal_eval(file_reader)
-    table = dynamodb_client.Table('user')
-    table.put_item(Item=file_reader)
-    return "success"
-
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+    response = client.subscribe(
+        TopicArn=os.environ['TopicArn'],
+        Protocol='email',
+        Endpoint=email,
+        Attributes={
+            'FilterPolicy': {'diet': diet}
+        },
+        )
 
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world",
+            "message": "success",
             # "location": ip.text.replace("\n", "")
         }),
     }
